bf74b20a5fee94e3582f2f89ce47bb7b45c491fa
tpvasoya <tpvasoya@myseneca.ca>
2022-05-18 12:33:36 -0400

    Assignment 1

---
+++ server.js
@@ -0,0 +1,25 @@
+/*********************************************************************************
+*  WEB322 – Assignment1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Tapas Pareshbhai Vasoya
+*  Student ID: 155190200
+*  Date: 18/05/2022
+*
+*  Online (Heroku) URL: _______________________________________________________
+*
+********************************************************************************/ 
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Tapas Pareshbhai Vasoya - 155190200");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
